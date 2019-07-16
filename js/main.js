b5038e5367522427f33e4ce1360cef018cb9c185
Galina Shvets <galina@gmail.com>
2019-07-16 13:37:12 +0300

    -add block Skills.

--- js/main.js
+++ js/main.js
@@ -43,4 +43,60 @@ jQuery(function ($) {
     });
 
 
+
+
+
+
+    var elem1 = document.getElementById("bar-eighty");
+    var elem2 = document.getElementById("bar-ninety-seven");
+    var elem3 = document.getElementById("bar-sixty-five");
+    var elem4 = document.getElementById("bar-seventy-two");
+    var width = 2;
+    var id = setInterval(animat1, 100);
+    var id2 = setInterval(animat2, 100);
+    var id3 = setInterval(animat3, 100);
+    var id4 = setInterval(animat4, 100);
+    function animat1() {
+        if (width >= 80) {
+            clearInterval(id);
+        } else {
+            width++;
+            elem1.style.width = width +'%';
+            document.getElementById("span-value-eighty").innerHTML = width;
+        }
+    }
+    function animat2() {
+        if (width >= 97) {
+            clearInterval(id2);
+        } else {
+            width++;
+            elem2.style.width = width + '%';
+            document.getElementById("span-value-ninety-seven").innerHTML = width;
+        }
+    }
+    function animat3() {
+        if (width >= 65) {
+            clearInterval(id3);
+        } else {
+            width++;
+            elem3.style.width = width + '%';
+            document.getElementById("span-value-sixty-five").innerHTML = width;
+        }
+    }
+    function animat4() {
+        if (width >= 72) {
+            clearInterval(id4);
+        } else {
+            width++;
+            elem4.style.width = width + '%';
+            document.getElementById("span-value-seventy-two").innerHTML = width;
+        }
+    }
+    $(window).scroll(animat1);
+    $(window).scroll(animat2);
+    $(window).scroll(animat3);
+    $(window).scroll(animat4);
+
+
+
 });
